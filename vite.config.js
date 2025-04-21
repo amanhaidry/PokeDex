73e8bbeed2bf37689d5def27d32cd9691e947128
amanhaidry <amanhaidry9480@gmail.com>
2025-04-22 03:08:32 +0530

    Update base path in vite.config.js for production deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/', // Add this line to match the repository name
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/PokeDex/' : '/', // Use '/PokeDex/' for GitHub Pages
   plugins: [react()],
-})
+}))
