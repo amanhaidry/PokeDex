4a8272bc10a72e3e5ea8289b208af78b32947c7a
amanhaidry <amanhaidry9480@gmail.com>
2025-04-22 00:30:08 +0530

    Update deployment script and configure base path for Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PokeDex/', // Add this line to match the repository name
   plugins: [react()],
 })
